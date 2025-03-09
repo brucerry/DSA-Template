8bef78dde53fd256006446a159d38c24efc0171a
bruce <bruce.cheung111@gmail.com>
2025-03-09 15:38:57 +0800

    Update segment_sum.cc

--- Trees/segment_sum.cc
+++ Trees/segment_sum.cc
@@ -17,7 +17,7 @@ private:
             int mid = (start + end) / 2;
             build(arr, 2 * node + 1, start, mid);
             build(arr, 2 * node + 2, mid + 1, end);
-            tree[node] = tree[2 * node + 1] + tree[2 * node + 2]; // Sum instead of max
+            tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
         }
     }
 
@@ -31,13 +31,13 @@ private:
             } else {
                 update(2 * node + 2, mid + 1, end, idx, value);
             }
-            tree[node] = tree[2 * node + 1] + tree[2 * node + 2]; // Maintain sum
+            tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
         }
     }
 
     int query(int node, int start, int end, int left, int right) {
         if (right < start || end < left) {
-            return 0; // Neutral sum value
+            return 0;
         }
         if (left <= start && end <= right) {
             return tree[node];
